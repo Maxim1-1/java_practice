45fe57522638f62ac881e08be44872c498a1ec23
Maxim1-1 <kick52@yadnex.ru>
2023-11-05 14:07:45 +0300

    1.4 add task

--- src/main/java/com/Maxim/multithreading/Foo.java
+++ src/main/java/com/Maxim/multithreading/Foo.java
@@ -1,18 +1,59 @@
 package com.Maxim.multithreading;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Foo {
-    public synchronized void first() {
-        System.out.print("first");
+
+    ReentrantLock lock = new ReentrantLock();
+
+    Condition secondCondition = lock.newCondition();
+    Condition thirdCondition = lock.newCondition();
+    private boolean isFirstExecuted = false;
+    private boolean isSecondExecuted = false;
+
+
+    public void first() {
+        lock.lock();
+        try {
+            System.out.print("first");
+            isFirstExecuted = true;
+            secondCondition.signal();
+        } finally {
+            lock.unlock();
+        }
     }
 
-    public synchronized void second() {
-        System.out.print("second");
+    public void second() {
+        lock.lock();
+        try {
+            while (!isFirstExecuted) {
+                secondCondition.await();
+            }
+            System.out.print("second");
+            isSecondExecuted = true;
+            thirdCondition.signal();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            lock.unlock();
+        }
     }
 
-    public synchronized void third() {
-        System.out.print("third");
+    public void third() {
+        lock.lock();
+        try {
+            while (!isSecondExecuted){
+                thirdCondition.await();
+            }
+            System.out.print("third");
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            lock.unlock();
+        }
+
     }
+
+
 }
-
-
-
--- src/main/java/com/Maxim/multithreading/Run.java
+++ src/main/java/com/Maxim/multithreading/Run.java
@@ -2,25 +2,17 @@ package com.Maxim.multithreading;
 
 public class Run {
     public static void main(String[] args) {
+
+
         Foo foo = new Foo();
-        Thread thread1 = new Thread(()->{
+        Thread thread1 = new Thread(() -> {
             foo.first();
         });
-        Thread thread2 = new Thread(()->{
-            try {
-                thread1.join();
-                foo.second();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+        Thread thread2 = new Thread(() -> {
+            foo.second();
         });
-        Thread thread3 = new Thread(()->{
-            try {
-                thread2.join();
-                foo.third();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+        Thread thread3 = new Thread(() -> {
+            foo.third();
         });
 
 
